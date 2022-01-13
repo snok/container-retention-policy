f46ddcda2fb4295373b347a2606b53a4a8c5b85f
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-01-13 22:12:33 +0100

    refactor: Add pydantic for input validation

--- main_tests.py
+++ main_tests.py
@@ -4,24 +4,22 @@ from functools import partial
 from unittest.mock import AsyncMock, Mock
 
 import pytest as pytest
-from dateparser import parse
 from httpx import AsyncClient
+from pydantic import ValidationError
 
 import main
 from main import (
     AccountType,
     ImageName,
     Inputs,
-    TimestampType,
     delete_org_package_versions,
     delete_package_versions,
     get_and_delete_old_versions,
-    get_image_version_tags,
     list_org_package_versions,
     list_package_versions,
 )
 from main import main as main_
-from main import parse_image_names, post_deletion_output, validate_inputs
+from main import post_deletion_output
 
 mock_response = Mock()
 mock_response.json.return_value = []
@@ -45,7 +43,9 @@ async def test_list_package_version():
 
 @pytest.mark.asyncio
 async def test_delete_org_package_version():
-    await delete_org_package_versions(org_name='test', image_name=ImageName('test', 'test'), http_client=mock_http_client, version_id=123)
+    await delete_org_package_versions(
+        org_name='test', image_name=ImageName('test', 'test'), http_client=mock_http_client, version_id=123
+    )
 
 
 @pytest.mark.asyncio
@@ -55,65 +55,48 @@ async def test_delete_package_version():
 
 def test_post_deletion_output(capsys):
     # Happy path
-    post_deletion_output(mock_response, image_name=ImageName('test', 'test'), version_id=123)
+    post_deletion_output(response=mock_response, image_name=ImageName('test', 'test'), version_id=123)
     captured = capsys.readouterr()
     assert captured.out == 'Deleted old image: test:123\n'
 
     # Bad response
-    post_deletion_output(mock_bad_response, image_name=ImageName('test', 'test'), version_id=123)
+    post_deletion_output(response=mock_bad_response, image_name=ImageName('test', 'test'), version_id=123)
     captured = capsys.readouterr()
     assert captured.out != 'Deleted old image: test:123\n'
 
 
-def test_inputs_dataclass():
-    personal = Inputs(
-        parsed_cutoff=parse('an hour ago utc'),
-        timestamp_type=TimestampType('created_at'),
-        account_type=AccountType('personal'),
-        untagged_only=False,
-        skip_tags=[],
-        keep_at_least=0,
-        filter_tags=[],
-        filter_include_untagged=True,
-    )
-    assert personal.is_org is False
-    assert personal.list_package_versions == list_package_versions
-    assert personal.delete_package == delete_package_versions
-
-    org = Inputs(
-        parsed_cutoff=parse('an hour ago utc'),
-        timestamp_type=TimestampType('created_at'),
-        account_type=AccountType('org'),
-        org_name='abcorp',
-        untagged_only=False,
-        skip_tags=[],
-        keep_at_least=0,
-        filter_tags=[],
-        filter_include_untagged=True,
-    )
-    assert org.is_org is True
-    assert isinstance(org.list_package_versions, partial)
-    assert isinstance(org.delete_package, partial)
+input_defaults = {
+    'image_names': 'a,b',
+    'cut_off': 'an hour ago utc',
+    'timestamp_to_use': 'created_at',
+    'untagged_only': 'false',
+    'skip_tags': '',
+    'keep_at_least': '0',
+    'filter_tags': '',
+    'filter_include_untagged': 'true',
+    'token': 'test',
+    'account_type': 'personal',
+}
 
 
-def test_get_image_version_tags():
-    assert (
-        get_image_version_tags(
-            {
-                'metadata': {'container': {'tags': []}},
-            }
-        )
-        == []
-    )
-    assert (
-        get_image_version_tags(
-            {
-                'metadata': {'container': {'tags': ['a']}},
-            }
-        )
-        == ['a']
-    )
-    assert get_image_version_tags({'metadata': {}}) == []
+def _create_inputs_model(**kwargs):
+    """
+    Little helper method, to help us instantiate working Inputs models.
+    """
+
+    return Inputs(**(input_defaults | kwargs))
+
+
+def test_inputs_model():
+    personal = _create_inputs_model(account_type='personal')
+    assert (personal.account_type == AccountType.ORG) is False
+    assert main.GithubAPI.list_package_versions(personal.account_type, personal.org_name) == list_package_versions
+    assert main.GithubAPI.delete_package(personal.account_type, personal.org_name) == delete_package_versions
+
+    org = _create_inputs_model(account_type='org')
+    assert (org.account_type == AccountType.ORG) is True
+    assert isinstance(main.GithubAPI.list_package_versions(org.account_type, org.org_name), partial)
+    assert isinstance(main.GithubAPI.delete_package(org.account_type, org.org_name), partial)
 
 
 class TestGetAndDeleteOldVersions:
@@ -129,210 +112,178 @@ class TestGetAndDeleteOldVersions:
             'url': 'https://api.github.com/orgs/org-name/packages/container/image-name/versions/1234567',
         }
     ]
-    valid_inputs = {
-        'parsed_cutoff': parse('an hour ago utc'),
-        'timestamp_type': TimestampType('created_at'),
-        'account_type': AccountType('personal'),
-        'untagged_only': False,
-        'skip_tags': [],
-        'keep_at_least': '0',
-        'filter_tags': [],
-        'filter_include_untagged': True,
-    }
 
     @staticmethod
-    async def _mock_list_package_versions(data, *args):
-        return data
+    def _mock_list_package_versions(data, *args, **kwargs):
+        """
+        This isn't trying to match the signature of the code we're mocking.
+
+        Rather, we're just hacking this together, to return the data we want.
+        """
+
+        async def _(*args, **kwargs):
+            return data
+
+        return _
 
     @pytest.mark.asyncio
-    async def test_delete_package(self, capsys):
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, self.valid_data)
-        inputs = Inputs(**self.valid_inputs)
-
+    async def test_delete_package(self, mocker, capsys):
+        mocker.patch.object(
+            main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, self.valid_data)
+        )
+        inputs = _create_inputs_model()
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'Deleted old image: a:1234567\n'
 
     @pytest.mark.asyncio
-    async def test_keep_at_least(self, capsys):
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, self.valid_data)
-        inputs = Inputs(**self.valid_inputs | {'keep_at_least': 1})
-
+    async def test_keep_at_least(self, mocker, capsys):
+        mocker.patch.object(
+            main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, self.valid_data)
+        )
+        inputs = _create_inputs_model(keep_at_least=1)
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'No more versions to delete for a\n'
 
     @pytest.mark.asyncio
-    async def test_not_beyond_cutoff(self, capsys):
-        data = [
+    async def test_not_beyond_cutoff(self, mocker, capsys):
+
+        response_data = [
             {
                 'created_at': str(datetime.now(timezone(timedelta(hours=1)))),
                 'id': 1234567,
             }
         ]
-
-        Inputs.list_package_versions = partial(
-            self._mock_list_package_versions,
-            data,
+        mocker.patch.object(
+            main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, response_data)
         )
-        inputs = Inputs(**self.valid_inputs)
-
+        inputs = _create_inputs_model()
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'No more versions to delete for a\n'
 
     @pytest.mark.asyncio
-    async def test_missing_timestamp(self, capsys):
+    async def test_missing_timestamp(self, mocker, capsys):
         data = [{'created_at': '', 'id': 1234567}]
-
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, data)
-        inputs = Inputs(**self.valid_inputs)
-
+        mocker.patch.object(main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, data))
+        inputs = _create_inputs_model()
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
-        assert captured.out == 'Skipping image version 1234567. Unable to parse timestamps.\nNo more versions to delete for a\n'
+        assert (
+            captured.out
+            == 'Skipping image version 1234567. Unable to parse timestamps.\nNo more versions to delete for a\n'
+        )
 
     @pytest.mark.asyncio
-    async def test_empty_list(self, capsys):
+    async def test_empty_list(self, mocker, capsys):
         data = []
-
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, data)
-        inputs = Inputs(**self.valid_inputs)
-
+        mocker.patch.object(main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, data))
+        inputs = _create_inputs_model()
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'No more versions to delete for a\n'
 
     @pytest.mark.asyncio
-    async def test_skip_tags(self, capsys):
+    async def test_skip_tags(self, mocker, capsys):
         data = deepcopy(self.valid_data)
         data[0]['metadata'] = {'container': {'tags': ['abc', 'bcd']}}
-
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, data)
-        inputs = Inputs(**self.valid_inputs | {'skip_tags': ['abc']})
-
+        mocker.patch.object(main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, data))
+        inputs = _create_inputs_model(skip_tags='abc')
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'No more versions to delete for a\n'
 
     @pytest.mark.asyncio
-    async def test_skip_tags_wildcard(self, capsys):
+    async def test_skip_tags_wildcard(self, mocker, capsys):
         data = deepcopy(self.valid_data)
         data[0]['metadata'] = {'container': {'tags': ['v1.0.0', 'abc']}}
-
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, data)
-        inputs = Inputs(**self.valid_inputs | {'skip_tags': ['v*']})
-
+        mocker.patch.object(main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, data))
+        inputs = _create_inputs_model(skip_tags='v*')
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'No more versions to delete for a\n'
 
     @pytest.mark.asyncio
-    async def test_untagged_only(self, capsys):
+    async def test_untagged_only(self, mocker, capsys):
         data = deepcopy(self.valid_data)
         data[0]['metadata'] = {'container': {'tags': ['abc', 'bcd']}}
-
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, data)
-        inputs = Inputs(**self.valid_inputs | {'untagged_only': 'true'})
-
+        mocker.patch.object(main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, data))
+        inputs = _create_inputs_model(untagged_only='true')
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'No more versions to delete for a\n'
 
     @pytest.mark.asyncio
-    async def test_filter_tags(self, capsys):
+    async def test_filter_tags(self, mocker, capsys):
         data = deepcopy(self.valid_data)
         data[0]['metadata'] = {'container': {'tags': ['sha-deadbeef', 'edge']}}
-
-        Inputs.list_package_versions = partial(self._mock_list_package_versions, data)
-        inputs = Inputs(**self.valid_inputs | {'filter_tags': ['sha-*']})
-
+        mocker.patch.object(main.GithubAPI, 'list_package_versions', partial(self._mock_list_package_versions, data))
+        inputs = _create_inputs_model(filter_tags='sha-*')
         await get_and_delete_old_versions(image_name=ImageName('a', 'a'), inputs=inputs, http_client=mock_http_client)
-
         captured = capsys.readouterr()
         assert captured.out == 'Deleted old image: a:1234567\n'
 
 
 def test_inputs_bad_account_type():
-    defaults = {
-        'account_type': 'org',
-        'org_name': 'test',
-        'timestamp_type': 'updated_at',
-        'cut_off': '2 hours ago UTC',
-        'untagged_only': False,
-        'skip_tags': None,
-        'keep_at_least': 0,
-        'filter_tags': None,
-        'filter_include_untagged': True,
-    }
-
     # Account type
-    validate_inputs(**defaults | {'account_type': 'personal'})
-    validate_inputs(**defaults | {'account_type': 'org'})
-    with pytest.raises(ValueError, match="'' is not a valid AccountType"):
-        validate_inputs(**defaults | {'account_type': ''})
+    _create_inputs_model(account_type='personal')
+    _create_inputs_model(account_type='org')
+    with pytest.raises(ValidationError, match='is not a valid enumeration member'):
+        _create_inputs_model(account_type='')
 
     # Org name
-    validate_inputs(**defaults | {'org_name': '', 'account_type': 'personal'})
+    _create_inputs_model(org_name='', account_type='personal')
     with pytest.raises(ValueError, match='org-name is required when account-type is org'):
-        validate_inputs(**defaults | {'org_name': ''})
+        _create_inputs_model(org_name='', account_type='org')
 
     # Timestamp type
-    validate_inputs(**defaults | {'timestamp_type': 'updated_at'})
-    validate_inputs(**defaults | {'timestamp_type': 'created_at'})
-    with pytest.raises(ValueError, match="'wat' is not a valid TimestampType"):
-        validate_inputs(**defaults | {'timestamp_type': 'wat'})
+    _create_inputs_model(timestamp_to_use='updated_at')
+    _create_inputs_model(timestamp_to_use='created_at')
+    with pytest.raises(ValueError, match=' value is not a valid enumeration mem'):
+        _create_inputs_model(timestamp_to_use='wat')
 
     # Cut-off
-    validate_inputs(**defaults | {'cut_off': '21 July 2013 10:15 pm +0500'})
-    validate_inputs(**defaults | {'cut_off': '12/12/12 PM EST'})
+    _create_inputs_model(cut_off='21 July 2013 10:15 pm +0500')
+    _create_inputs_model(cut_off='12/12/12 PM EST')
     with pytest.raises(ValueError, match='Timezone is required for the cut-off'):
-        validate_inputs(**defaults | {'cut_off': '12/12/12'})
+        _create_inputs_model(cut_off='12/12/12')
     with pytest.raises(ValueError, match="Unable to parse 'lolol'"):
-        validate_inputs(**defaults | {'cut_off': 'lolol'})
+        _create_inputs_model(cut_off='lolol')
 
     # Untagged only
     for i in ['true', 'True', '1']:
-        assert validate_inputs(**defaults | {'untagged_only': i}).untagged_only is True
+        assert _create_inputs_model(untagged_only=i).untagged_only is True
     for j in ['False', 'false', '0']:
-        assert validate_inputs(**defaults | {'untagged_only': j}).untagged_only is False
-    assert validate_inputs(**defaults | {'untagged_only': False}).untagged_only is False
+        assert _create_inputs_model(untagged_only=j).untagged_only is False
+    assert _create_inputs_model(untagged_only=False).untagged_only is False
 
     # Skip tags
-    assert validate_inputs(**defaults | {'skip_tags': 'a'}).skip_tags == ['a']
-    assert validate_inputs(**defaults | {'skip_tags': 'a,b'}).skip_tags == ['a', 'b']
-    assert validate_inputs(**defaults | {'skip_tags': 'a , b  ,c'}).skip_tags == ['a', 'b', 'c']
+    assert _create_inputs_model(skip_tags='a').skip_tags == ['a']
+    assert _create_inputs_model(skip_tags='a,b').skip_tags == ['a', 'b']
+    assert _create_inputs_model(skip_tags='a , b  ,c').skip_tags == ['a', 'b', 'c']
 
     # Keep at least
-    with pytest.raises(ValueError, match='keep-at-least must be 0 or positive'):
-        validate_inputs(**defaults | {'keep_at_least': '-1'})
+    with pytest.raises(ValueError, match='ensure this value is greater than or equal to 0'):
+        _create_inputs_model(keep_at_least='-1')
 
     # Filter tags
-    assert validate_inputs(**defaults | {'filter_tags': 'a'}).filter_tags == ['a']
-    assert validate_inputs(**defaults | {'filter_tags': 'sha-*,latest'}).filter_tags == ['sha-*', 'latest']
-    assert validate_inputs(**defaults | {'filter_tags': 'sha-* , latest'}).filter_tags == ['sha-*', 'latest']
+    assert _create_inputs_model(filter_tags='a').filter_tags == ['a']
+    assert _create_inputs_model(filter_tags='sha-*,latest').filter_tags == ['sha-*', 'latest']
+    assert _create_inputs_model(filter_tags='sha-* , latest').filter_tags == ['sha-*', 'latest']
 
     # Filter include untagged
     for i in ['true', 'True', '1', True]:
-        assert validate_inputs(**defaults | {'filter_include_untagged': i}).filter_include_untagged is True
+        assert _create_inputs_model(filter_include_untagged=i).filter_include_untagged is True
     for j in ['False', 'false', '0', False]:
-        assert validate_inputs(**defaults | {'filter_include_untagged': j}).filter_include_untagged is False
-    assert validate_inputs(**defaults | {'filter_include_untagged': ''}).filter_include_untagged is True
+        assert _create_inputs_model(filter_include_untagged=j).filter_include_untagged is False
 
 
 def test_parse_image_names():
-    assert parse_image_names('a') == [ImageName('a', 'a')]
-    assert parse_image_names('a,b') == [ImageName('a', 'a'), ImageName('b', 'b')]
-    assert parse_image_names('  a  ,  b ') == [ImageName('a', 'a'), ImageName('b', 'b')]
-    assert parse_image_names('a/a') == [ImageName('a/a', 'a%2Fa')]
+    assert _create_inputs_model(image_names='a').image_names == [ImageName('a', 'a')]
+    assert _create_inputs_model(image_names='a,b').image_names == [ImageName('a', 'a'), ImageName('b', 'b')]
+    assert _create_inputs_model(image_names='  a  ,  b ').image_names == [ImageName('a', 'a'), ImageName('b', 'b')]
+    assert _create_inputs_model(image_names='a/a').image_names == [ImageName('a/a', 'a%2Fa')]
 
 
 @pytest.mark.asyncio
@@ -345,8 +296,13 @@ async def test_main(mocker):
             'account_type': 'org',
             'org_name': 'test',
             'image_names': 'a,b,c',
-            'timestamp_type': 'updated_at',
+            'timestamp_to_use': 'updated_at',
             'cut_off': '2 hours ago UTC',
-            'token': 'abc',
+            'untagged_only': 'false',
+            'skip_tags': '',
+            'keep_at_least': '0',
+            'filter_tags': '',
+            'filter_include_untagged': 'true',
+            'token': 'test',
         }
     )
